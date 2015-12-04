b1486efd389f19e060b97cf69fe67055f341647b
Brian McSheehy <brian@Brians-MBP.attlocal.net>
2015-12-04 16:00:38 -0600

    updating install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# Copyright 2015 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#####################################################
 
 ADD_IPSEC_DIR_SRC="https://raw.githubusercontent.com/bmcsheehy/softlayer_vyatta/master/add_ipsec_conf"
 ADD_IPSEC_DIR_LOCAL="/opt/vyatta/sbin/add_ipsec_conf"
@@ -41,5 +57,4 @@ fi
 
 echo "* executing update"
 exec $IPSEC_SYMLINK
-
-echo "* success!"
+echo "* complete!"
